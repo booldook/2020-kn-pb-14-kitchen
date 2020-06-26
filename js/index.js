9d2d2dd1be6359cfa7a75092dbbb9b3f004a698b
booldook <booldook@gmail.com>
2020-06-26 12:23:03 +0900

    header 만드는 중

--- js/index.js
+++ js/index.js
@@ -7,7 +7,33 @@
 
 
 /**************** 이벤트 콜백 *****************/
+function onResize() {
+	var wid = $(this).innerWidth();
+	if(wid > 991) {
 
+	}
+	else if(wid > 767) {
+
+	}
+	else if(wid > 576) {
+
+	}
+	else {
+		
+	}
+}
+
+function onScroll() {
+
+}
+
+function onBarsClick() {
+	$(".mo-wrap").stop().slideToggle(300);
+}
 
 
 /**************** 이벤트 등록 *****************/
+$(window).resize(onResize).trigger("resize");
+$(window).scroll(onScroll).trigger("scroll");
+
+$(".mo-bars").click(onBarsClick);
